a1c84813e630375acb49a1a4f0d72b96453e104f
Aneeshbs44 <anish107655@gmail.com>
2024-12-24 13:16:11 +0530

    Added Navigation Independent tree so that there is no confusion between navigation as expo has default navigation present in the form of tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,36 @@
+// In index.tsx
+import "react-native-gesture-handler";
 import React from "react";
-import { Stack } from "expo-router";
+import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../../src/Login"
+import Dashboard from "../../src/Dashboard"
+import Detail from "../../src/Detail"
 
-export default function Layout() {
+const Stack = createStackNavigator();
+
+export default function App() {
   return (
-    <Stack>
-      <Stack.Screen 
-        name="Login" 
-        options={{ 
-          headerShown: false 
-        }} 
-      />
-      <Stack.Screen 
-        name="detail" 
-        options={{ 
-          headerShown: false 
-        }} 
-      />
-      <Stack.Screen 
-        name="dashboard" 
-        options={{ 
-          headerShown: false 
-        }} 
-      />
-    </Stack>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
